af8fee2a915f77c2fec8eb3abf15c1cb91dcd872
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-06-23 16:10:20 +0300

    first try

---
+++ Rabbit/Rabbit/MammalRabbit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rabbit
+{
+    public enum MammalType
+    {
+        Rabbit,
+        Name,
+        BirthDay,
+        Moves,
+        Sleeps,
+        Eats,
+    }
+
+    class MammalRabbit
+    {
+        public string Moves { get; set; }
+        public string Sleeps { get; set; }
+        public string Eats { get; set; }
+        public DateTime BirthDate { get; }
+        
+        
+        public string Name { get; }
+        public string Eyes { get; }
+        public string Fur { get; }
+        public string Gender { get; }
+
+
+
+        public MammalRabbit( MammalType type, string name, string eyes, string fur, string gender,  DateTime birthdate,  string moves, string sleeps, string eats)
+        {
+            
+            Name = name;
+            Eyes = eyes;
+            Fur = fur;
+            Gender = gender;
+            BirthDate = birthdate;
+            
+            Moves = moves;
+            Sleeps = sleeps;
+            Eats = eats;
+        }
+       
+
+
+    }
+
+
+
+
+}
+    
+
--- Rabbit/Rabbit/Program.cs
+++ Rabbit/Rabbit/Program.cs
@@ -7,6 +7,28 @@ namespace Rabbit
         static void Main(string[] args)
         {
             
+          
+            MammalRabbit myRabbit = new MammalRabbit(MammalType.Rabbit,"Cinnabon", "Blue", "Brown", "Female",  new DateTime(2016,03,24),"Jumps", "With one eye open", "Carrots");
+            Console.WriteLine($"The {MammalType.Rabbit} is a mammal!");
+            Console.WriteLine($"The gender of the rabbit is {myRabbit.Gender}");
+            Console.WriteLine($"The rabbit's name is {myRabbit.Name}");
+            Console.WriteLine($"Cinnabon has {myRabbit.Eyes} eyes");
+            Console.WriteLine($"Her fur's colour is {myRabbit.Fur}");
+            Console.WriteLine($"She {myRabbit.Moves} all over the place");
+            Console.WriteLine($"She sleeps {myRabbit.Sleeps}");
+            Console.WriteLine($"Cinnabon eats a lot of {myRabbit.Eats}");
+            Console.WriteLine($"She was born on {myRabbit.BirthDate}");
+
+
+
+            DateTime today = DateTime.Now;
+            TimeSpan span = today - new DateTime(2016,03,24);
+            DateTime age = DateTime.MinValue + span;
+            int years = age.Year - 1;
+            int months = age.Month - 1;
+            Console.Write($"And now her age is :{years} years and {months} months", years, months);
+
         }
+
     }
 }
